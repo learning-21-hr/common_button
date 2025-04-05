ef5a6dbcb829aa9344e4192ea4b7079c73efff8c
“Abhijeet” <abhijeet.sen@techaheadcorp.com>
2025-04-06 00:17:52 +0530

    Common Button initial commit.

--- lib/common_button.dart
+++ lib/common_button.dart
@@ -1,7 +1,18 @@
 library common_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CommonButton extends StatefulWidget {
+  const CommonButton({required this.onPressed, super.key});
+  final void Function()? onPressed;
+
+  @override
+  State<CommonButton> createState() => _CommonButtonState();
+}
+
+class _CommonButtonState extends State<CommonButton> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: widget.onPressed, icon: const Icon(Icons.add));
+  }
 }
--- test/common_button_test.dart
+++ test/common_button_test.dart
@@ -1,12 +1,13 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:common_button/common_button.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// import 'package:flutter_test/flutter_test.dart';
+//
+// import 'package:common_button/common_button.dart';
+//
+// void main() {
+//   test('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
+// TODO :: Need to write test case of the button
